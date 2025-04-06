0874e53dc9946dd9652e4938072ba95eea10997e
Himanshu Jha <jha.himanshu966@gmail.com>
2025-04-06 13:32:16 +0530

    add eslint

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
 function welcome(name: string) {
     console.log(`Welcome to pizzaHub ${name}`)
+    const user = {
+        name: 'Himanshu',
+    }
+    const u1 = user.name
+    return name
 }
-
 welcome('Himanshu')
